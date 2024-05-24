b3927eceb2f52b37879cbf300680e326d11a3936
Shuyesh <shuyeshacharya@gmail.com>
2024-05-24 11:57:54 +0545

    Made project showable

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const contentContainer = document.querySelector(".content");
 const inputs = document.querySelectorAll("input");
 const gridValue = document.querySelector(".rangevalue")
 const buttons = document.querySelectorAll(".controls");
-console.log(buttons);
 
 let boxes ;
 let text ;
@@ -11,12 +10,22 @@ let isOver = true;
 let toggle;
 let color;
 
+makeBoxes();
+
 inputs[0].addEventListener('input' ,()=>{
     choice(toggle);
 });
 inputs[1].addEventListener("input" ,()=>{
     makeBoxes();
 });
+
+function getRandomValue(){
+    return Math.floor((Math.random() * 255)).toString(16);
+}
+function rgb(){
+    return `#${getRandomValue()}${getRandomValue()}${getRandomValue()}`;
+}
+
 
 function makeBoxes(){
     while (contentContainer.hasChildNodes()){
@@ -42,28 +51,65 @@ function makeBoxes(){
     boxes = document.querySelectorAll(".box");
     choice(toggle);
     buttons.forEach((button) => {
-        button.addEventListener('click', (e) => {
+        button.addEventListener('click', () => {
             toggle = button.name;
             choice(toggle);
         });
     });
 }
 
-makeBoxes();
+
 
 function choice(toggle){
     if(toggle === 'erase'){
+        buttons.forEach((button) =>{
+            button.classList.remove("clicked");
+        });
         writeOnScreen("#FFFFFF");
+        buttons[1].classList.add("clicked");
     }else if(toggle === "clear"){
+        buttons.forEach((button) =>{
+            button.classList.remove("clicked");
+        });
         boxes.forEach((box) =>{
             box.style.backgroundColor = "#FFFFFF";
         });
-        toggle = 'draw';
+        writeOnScreen("#FFFFFF");
+        buttons[2].classList.add("clicked");
+    }else if (toggle === "random"){
+        buttons.forEach((button) =>{
+            button.classList.remove("clicked");
+        });
+        boxes.forEach((box) =>{
+            box.addEventListener('mouseover', (e) => {
+                isOver = true;
+                if(isPressed && isOver){
+                    e.target.style.backgroundColor = rgb();
+                }
+            });
+            box.addEventListener('click' , (e)=>{
+                e.target.style.backgroundColor = rgb();
+
+            });
+            box.addEventListener('mousedown', () => {
+                isPressed = true;
+            });
+            
+            box.addEventListener('mouseup', () => {
+                isPressed = false;
+            });
+        });
+        buttons[3].classList.add("clicked");
     }
     else{
+        buttons.forEach((button) =>{
+            button.classList.remove("clicked");
+        });
         writeOnScreen(inputs[0].value);
+        buttons[0].classList.add("clicked");
     }
 }
+
 function writeOnScreen(value){
     boxes.forEach((box) =>{
         box.addEventListener('mouseover', (e) => {
